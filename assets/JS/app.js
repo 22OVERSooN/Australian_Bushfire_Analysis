fbf3f90cb53fe80639b95ff0b69bf47d22cd05d0
22 (OVER S∞∞N) <75764401+22OVERSooN@users.noreply.github.com>
2021-09-06 17:27:43 +1000

    add code for bar plot

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,20 +1,67 @@
-function init() {
-    // select the dropdown area for bar chart
-    var dropdownBar = d3.select("#selDataset2");
 
-    //read the json data file
-    const stationNames = [];
-    d3.json("../../Resources/Json/test.json").then((data) => {
+function init(){
+    //select the dropdown area for bar chart
+    var dropdownBar1 = d3.select("#selDataset2")
 
-        const stationName = data[0];
+    const stationNames = []; 
+    //read the json file
+    d3.json("../../Resources/Json/aus_weather_barchart.json").then((datas) => {  
+        //render through the data set
+        datas.forEach(function(data){
+            const stationName = data['State-Station']
 
-        if (stationNames.includes(stationName))
-        return false;
+            if (stationNames.includes(stationName))
+            return false;
 
-        stationNames.push(stationName);
-        dropdownBar+='<option value="' + stationName + '">' + stationName + '</option>';
-
+            //append the dropdown list to options
+            stationNames.push(stationName);
+            dropdownBar1.append("option").text(stationName).property("value");
+        })
+        console.log(stationNames)
+        
     })
+    //drawPlot(datas[0]['State-Station'])
 }
 init();
+
+function drawPlot(Names){
+    //create a list to restore the maxtemp, rain and data
+    const MaxTemp = [];
+    const Rain = []
+    const Date = []
+
+    
+    d3.json("../../Resources/Json/aus_weather_barchart.json").then((datas) => {  
+        //render through the data set
+        datas.forEach(function(d){
+            if(d['State-Station'] === Names){
+                Date.push(d['Date'])
+                MaxTemp.push(d['Max Temp'])
+            }
+            // if(data['State-Station'] === Names)
+            //     Date.push(data)
+            //     MaxTemp.push(data['Max Temp'])
+        });
+        console.log(MaxTemp)
+        console.log(Date)
+
+        var trace = {
+            x:Date,
+            y:MaxTemp,
+            type:"bar",
+        };
+        var data = [trace];
+    
+        var layout = {
+            font:{family:"Arial Rounded MT Bold"},
+            title:"Max Temp Monthly 05/2019-06/2020",
+            yaxis:{ title : "Max Temp"},
+            xaxis:{ title : "Date"},
+          
+        }
+        Plotly.newPlot("barplot",data,layout)
+    });
+};
+drawPlot("ACT-Canberra Airport ")
+
 
