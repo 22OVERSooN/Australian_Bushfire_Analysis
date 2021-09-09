cc862ccf435524fa70ffabd197146dfe42215a8b
Thgiliwt <83489530+Thgiliwt@users.noreply.github.com>
2021-09-09 20:15:33 +1000

    combined echo and rachels part in app.py file

--- harmfulgas/app.py
+++ harmfulgas/app.py
@@ -14,6 +14,39 @@ client = pymongo.MongoClient(conn)
 db11 = client.bushfires
 slidebar = db11.harmful_gas
 
+# connect to mongo db and collection - below are from echo & rachel==============
+# db2 = client.barchart
+# weatherbar = db2.aus_weather_barchart
+#================================================================================
+
+#################################################
+# Flask Routes
+#################################################
+
+# @app.route('/january', methods=['GET'])
+# @cross_origin()
+# def foo():
+#     output = []
+#     for s in produce.find():
+#         if  s['acq_date'] < "2019-02-01":
+#             output.append({'date': s['acq_date'],
+#                             'latitude': s['latitude'],
+#                             'longitude': s['longitude'],
+#                             'brightness': s['brightness']})
+#     return jsonify(output)
+
+#==================echo & rachel========================
+# @app.route('/barsnscatters')
+# def barsnscatters():
+
+#     bar_data = []
+#     for bdata in weatherbar.find():
+#         bdata.pop('_id') 
+#         bdata=bdata.copy()
+#         bar_data.append(bdata)
+#     return jsonify(bar_data)
+#========================================================
+
 @app.route("/")
 def welcome():
     return (
