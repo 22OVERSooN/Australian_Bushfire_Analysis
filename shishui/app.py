41245d3f3fb0364fb8c55768649ad1d5f80d5d79
User <c.j.dancewilson@outlook.com>
2021-09-11 21:12:48 +1000

    UPdated app.py with monthly data routes.

--- shishui/app.py
+++ shishui/app.py
@@ -6,9 +6,26 @@ import pymongo
 
 #use flask_pymongo to set up mongo connection
 app = Flask(__name__)
-CORS(app)
+cors = CORS(app, resources={r"/january": {"origins": "*"}})
+cors = CORS(app, resources={r"/february": {"origins": "*"}})
+cors = CORS(app, resources={r"/march": {"origins": "*"}})
+cors = CORS(app, resources={r"/april": {"origins": "*"}})
+cors = CORS(app, resources={r"/may": {"origins": "*"}})
+cors = CORS(app, resources={r"/june": {"origins": "*"}})
+cors = CORS(app, resources={r"/july": {"origins": "*"}})
+cors = CORS(app, resources={r"/august": {"origins": "*"}})
+cors = CORS(app, resources={r"/september": {"origins": "*"}})
+cors = CORS(app, resources={r"/october": {"origins": "*"}})
+cors = CORS(app, resources={r"/november": {"origins": "*"}})
+cors = CORS(app, resources={r"/december": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
+app.config["MONGO_URI"] = "mongodb://localhost:27017/bushfires"
+
 conn = "mongodb://localhost:27017"
 client = pymongo.MongoClient(conn)
+
+db11 = client.bushfires
+produce = db11.fire
 
 # connect to mongo db and collection
 db11 = client.bushfires
@@ -23,17 +40,165 @@ weatherbar = db2.aus_weather_barchart
 # Flask Routes
 #################################################
 
-# @app.route('/january', methods=['GET'])
-# @cross_origin()
-# def foo():
-#     output = []
-#     for s in produce.find():
-#         if  s['acq_date'] < "2019-02-01":
-#             output.append({'date': s['acq_date'],
-#                             'latitude': s['latitude'],
-#                             'longitude': s['longitude'],
-#                             'brightness': s['brightness']})
-#     return jsonify(output)
+@app.route('/january', methods=['GET'])
+@cross_origin()
+def january():
+    output = []
+    for s in produce.find():
+        if  s['acq_date'] < "2019-02-01":
+            output.append({'date': s['acq_date'],
+                            'latitude': s['latitude'],
+                            'longitude': s['longitude'],
+                            'brightness': s['brightness']})
+    return jsonify(output)
+
+@app.route('/february', methods=['GET'])
+@cross_origin()
+def february():
+    output = []
+    for s in produce.find():
+        if  s['acq_date'] >= "2019-02-01":
+            if s['acq_date'] < "2019-03-01":
+                output.append({'date': s['acq_date'],
+                                'latitude': s['latitude'],
+                                'longitude': s['longitude'],
+                                'brightness': s['brightness']})
+    return jsonify(output)
+
+
+@app.route('/march', methods=['GET'])
+@cross_origin()
+def march():
+    output = []
+    for s in produce.find():
+        if  s['acq_date'] >= "2019-03-01":
+            if s['acq_date'] < "2019-04-01":
+                output.append({'date': s['acq_date'],
+                                'latitude': s['latitude'],
+                                'longitude': s['longitude'],
+                                'brightness': s['brightness']})
+    return jsonify(output)
+
+@app.route('/april', methods=['GET'])
+@cross_origin()
+def april():
+    output = []
+    for s in produce.find():
+        if  s['acq_date'] >= "2019-04-01":
+            if s['acq_date'] < "2019-05-01":
+                output.append({'date': s['acq_date'],
+                                'latitude': s['latitude'],
+                                'longitude': s['longitude'],
+                                'brightness': s['brightness']})
+    return jsonify(output)
+
+@app.route('/may', methods=['GET'])
+@cross_origin()
+def may():
+    output = []
+    for s in produce.find():
+        if  s['acq_date'] >= "2019-05-01":
+            if s['acq_date'] < "2019-06-01":
+                output.append({'date': s['acq_date'],
+                                'latitude': s['latitude'],
+                                'longitude': s['longitude'],
+                                'brightness': s['brightness']})
+    return jsonify(output)
+
+@app.route('/june', methods=['GET'])
+@cross_origin()
+def june():
+    output = []
+    for s in produce.find():
+        if  s['acq_date'] >= "2019-06-01":
+            if s['acq_date'] < "2019-07-01":
+                output.append({'date': s['acq_date'],
+                                'latitude': s['latitude'],
+                                'longitude': s['longitude'],
+                                'brightness': s['brightness']})
+    return jsonify(output)
+
+@app.route('/july', methods=['GET'])
+@cross_origin()
+def july():
+    output = []
+    for s in produce.find():
+        if  s['acq_date'] >= "2019-07-01":
+            if s['acq_date'] < "2019-08-01":
+                output.append({'date': s['acq_date'],
+                                'latitude': s['latitude'],
+                                'longitude': s['longitude'],
+                                'brightness': s['brightness']})
+    return jsonify(output)
+
+
+@app.route('/august', methods=['GET'])
+@cross_origin()
+def august():
+    output = []
+    for s in produce.find():
+        if  s['acq_date'] >= "2019-08-01":
+            if s['acq_date'] < "2019-09-01":
+                output.append({'date': s['acq_date'],
+                                'latitude': s['latitude'],
+                                'longitude': s['longitude'],
+                                'brightness': s['brightness']})
+    return jsonify(output)
+
+
+@app.route('/september', methods=['GET'])
+@cross_origin()
+def september():
+    output = []
+    for s in produce.find():
+        if  s['acq_date'] >= "2019-09-01":
+            if s['acq_date'] < "2019-10-01":
+                output.append({'date': s['acq_date'],
+                                'latitude': s['latitude'],
+                                'longitude': s['longitude'],
+                                'brightness': s['brightness']})
+    return jsonify(output)
+
+@app.route('/october', methods=['GET'])
+@cross_origin()
+def october():
+    output = []
+    for s in produce.find():
+        if  s['acq_date'] >= "2019-10-01":
+            if s['acq_date'] < "2019-11-01":
+                output.append({'date': s['acq_date'],
+                                'latitude': s['latitude'],
+                                'longitude': s['longitude'],
+                                'brightness': s['brightness']})
+    return jsonify(output)
+
+
+@app.route('/november', methods=['GET'])
+@cross_origin()
+def november():
+    output = []
+    for s in produce.find():
+        if  s['acq_date'] >= "2019-11-01":
+            if s['acq_date'] < "2019-12-01":
+                output.append({'date': s['acq_date'],
+                                'latitude': s['latitude'],
+                                'longitude': s['longitude'],
+                                'brightness': s['brightness']})
+    return jsonify(output)
+
+
+@app.route('/december', methods=['GET'])
+@cross_origin()
+def december():
+    output = []
+    for s in produce.find():
+        if  s['acq_date'] >= "2019-12-01":
+            if s['acq_date'] < "2020-01-01":
+                output.append({'date': s['acq_date'],
+                                'latitude': s['latitude'],
+                                'longitude': s['longitude'],
+                                'brightness': s['brightness']})
+    return jsonify(output)
 
 #==================echo & rachel========================
 
